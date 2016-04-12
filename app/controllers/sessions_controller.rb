cebe59fa2450d61ef7064ac0d573145360cf0174
Dimitry Besson <dimitrybesson@gmail.com>
2016-04-12 16:41:13 -0400

    Added flash alerts and notices

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, notice: 'Logged in!'
     else
-      flash[:error] = 'Failed to log in.'
+      flash[:error] = 'Invalid email or password.'
       render :new
     end
   end
